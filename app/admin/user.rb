a3a88ac19528909344980c17875e96c240acfb5a
Istiak Ahmed <istiak.aiub.08@gmail.com>
2016-03-01 01:48:40 +0100

    User model on active admin has been modified

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,6 +1,27 @@
 ActiveAdmin.register User do
 
-  permit_params :full_name, :email
+  form do |f|
+    f.inputs 'User Details' do
+      f.input :full_name
+      f.input :email
+      if f.object.new_record?
+        f.input :password
+        f.input :password_confirmation
+      end
+    end
+    f.actions
+  end
+
+  index do
+    selectable_column
+    column :full_name
+    id_column
+    column :email
+    column :last_sign_in_at
+    actions
+  end
+
+  permit_params :full_name, :email, :password, :password_confirmation
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
